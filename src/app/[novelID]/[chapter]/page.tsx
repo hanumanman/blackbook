67bbbdc215c3a1d64c2b409fe3dd17fd13ecf1ad
itztla <hanumanman37@gmail.com>
2024-08-29 12:09:24 +0700

    add suspense boundary + update next version to fix fetch error

--- src/app/[novelID]/[chapter]/page.tsx
+++ src/app/[novelID]/[chapter]/page.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/ui/button';
 import { getChapter } from '@/db/queries/selects';
 import { Navigation } from './components/Navigation';
+import { Suspense } from 'react';
 
 const ChapterPage = async ({
   params,
@@ -15,14 +15,22 @@ const ChapterPage = async ({
   const data = chapterData[0];
 
   return (
-    <div>
-      <p> Chapter {data.chapter_number}</p>
+    <div className="min-h-[100vh] px-12 py-16">
       <Navigation
         chapterNumber={parseInt(chapter)}
         novelID={parseInt(novelID)}
       />
-      <p className="text-xl font-bold">{data.chapter_name}</p>
-      <p className="whitespace-pre-wrap">{data.chapter_content}</p>
+      <Suspense
+        fallback={
+          <div className="w-full h-96 flex justify-center items-center">
+            Loading...
+          </div>
+        }
+      >
+        <p> Chapter {data.chapter_number}</p>
+        <p className="text-xl font-bold">{data.chapter_name}</p>
+        <p className="whitespace-pre-wrap">{data.chapter_content}</p>
+      </Suspense>
 
       <Navigation
         chapterNumber={parseInt(chapter)}
