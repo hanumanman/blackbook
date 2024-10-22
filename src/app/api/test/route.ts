68c143b7de22ef933d0dbe910cc3b97255b75fee
itztla <hanumanman37@gmail.com>
2024-10-22 23:53:46 +0700

    fix imports

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest) {
   const body = await request.json().catch(() => NextResponse.json({ message: 'Invalid JSON' }, { status: 400 }));
 
-  // return NextResponse.json({ message: 'Hello World!', body });
-  return NextResponse.json({ message: 'Hello Error!', body }, { status: 500 });
+  return NextResponse.json({ message: 'Hello World!', body });
+  // return NextResponse.json({ message: 'Hello Error!', body }, { status: 500 });
 }
