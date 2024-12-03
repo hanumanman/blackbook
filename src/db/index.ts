f5f24ae7387bc44509662d84f8835815b86b9a11
itztla <hanumanman37@gmail.com>
2024-12-03 19:45:14 +0700

    Squash merge develop into main

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,15 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
+const url = process.env.TURSO_DATABASE_URL;
+if (!url) throw new Error('Cannot find TURSO_DATABASE_URL in env');
+
+const authToken = process.env.TURSO_AUTH_TOKEN;
+if (!authToken) throw new Error('Cannot find TURSO_AUTH_TOKEN in env');
+
 const tursoClient = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url,
+  authToken,
 });
 
 export const db = drizzle(tursoClient);
