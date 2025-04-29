09359e712f88f710a017ff8f5e932e7307eab158
itztla <hanumanman37@gmail.com>
2025-04-29 16:05:02 +0700

    fix: fix light system theme

--- src/components/ModeToggler.tsx
+++ src/components/ModeToggler.tsx
@@ -13,12 +13,14 @@ import { useTheme } from 'next-themes';
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
+  const isLightTheme =
+    theme === 'light' || (theme === 'system' && window.matchMedia('(prefers-color-scheme: light)').matches);
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          {theme === 'light' ? (
+          {isLightTheme ? (
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           ) : (
             <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
